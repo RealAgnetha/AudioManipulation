3ccfa658ad5357e8f329cccb66f48a0e01852f30
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-11-16 22:07:47 +0100

    more cleanup

--- src/main.js
+++ src/main.js
@@ -9,9 +9,52 @@ let audioCtx;
 let track;
 
 const audioElement = document.querySelector("audio");
-const playButton = document.querySelector(".play-btn");
+const playButton = document.getElementById("play-btn");
+const visualizeBtn = document.getElementById("viz-btn");
 
-// play pause btn
+//for bandpass 
+const diff = higherBandThreshold - lowerBandThreshold;
+const centerFreq = higherBandThreshold - (1 / 2) * diff;
+
+
+function init() {
+    audioCtx = new AudioContext();
+
+    track = new MediaElementAudioSourceNode(audioCtx, {
+        mediaElement: audioElement,
+    });
+
+    //le filters
+    let lowFilter = new BiquadFilterNode(audioCtx, {type: 'lowshelf', frequency: lowerBandThreshold});
+    let midFilter = new BiquadFilterNode(audioCtx, {type: 'bandpass', frequency: centerFreq, Q: 1});
+    let highFilter = new BiquadFilterNode(audioCtx, {type: 'highshelf', frequency: higherBandThreshold})
+
+    Lows.oninput = () => lowFilter.gain.value = Lows.value;
+    Mids.oninput = () => midFilter.gain.value = Mids.value;
+    Highs.oninput = () => highFilter.gain.value = Highs.value;
+
+
+    // Volume
+    const gainNode = new GainNode(audioCtx);
+    const volumeControl = document.querySelector('[data-action="volume"]');
+    volumeControl.addEventListener(
+        "input",
+        () => {
+            gainNode.gain.value = volumeControl.value;
+        },
+        false
+    );
+
+    // audio graph
+    gainNode.connect(audioCtx.destination);
+    track.connect(lowFilter);
+    lowFilter.connect(midFilter);
+    midFilter.connect(highFilter);
+    highFilter.connect(gainNode);
+
+}
+
+// play pause btn 
 playButton.addEventListener(
     "click",
     () => {
@@ -49,48 +92,44 @@ audioElement.addEventListener(
     false
 );
 
-//for bandpass 
-const diff = higherBandThreshold - lowerBandThreshold;
-const centerFreq = higherBandThreshold - (1 / 2) * diff;
-
+//for visualisation
 const container = document.getElementById("visualisation");
 const canvas = document.getElementById("canvas1");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const canvasCtx = canvas.getContext('2d');
+let analyser;
 
-function init() {
-    audioCtx = new AudioContext();
-    track = new MediaElementAudioSourceNode(audioCtx, {
-        mediaElement: audioElement,
-    });
+visualizeBtn.addEventListener("click", function () {
 
-    //le filters
-    let lowFilter = new BiquadFilterNode(audioCtx, {type: 'lowshelf', frequency: lowerBandThreshold});
-    let midFilter = new BiquadFilterNode(audioCtx, {type: 'bandpass', frequency: centerFreq, Q: 1});
-    let highFilter = new BiquadFilterNode(audioCtx, {type: 'highshelf', frequency: higherBandThreshold})
+        analyser = audioCtx.createAnalyser();
 
-    Lows.oninput = () => lowFilter.gain.value = Lows.value;
-    Mids.oninput = () => midFilter.gain.value = Mids.value;
-    Highs.oninput = () => highFilter.gain.value = Highs.value;
+        track.connect(analyser);
+        analyser.connect(audioCtx.destination);
 
-    
-    // Volume
-    const gainNode = new GainNode(audioCtx);
-    const volumeControl = document.querySelector('[data-action="volume"]');
-    volumeControl.addEventListener(
-        "input",
-        () => {
-            gainNode.gain.value = volumeControl.value;
-        },
-        false
-    );
-    
-    
-    // audio graph
-    gainNode.connect(audioCtx.destination);
-    track.connect(lowFilter);
-    lowFilter.connect(midFilter);
-    midFilter.connect(highFilter);
-    highFilter.connect(gainNode);
+        analyser.fftSize = 2048;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
 
-}
+        const barWidth = canvas.width / bufferLength;
+        let barHeight;
+        let x = 0;
+
+        function visualize() {
+            x = 0;
+            canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+            analyser.getByteFrequencyData(dataArray);
+
+            for (let i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i];
+                canvasCtx.fillStyle = 'black';
+                canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+                x += barWidth;
+            }
+            requestAnimationFrame(visualize);
+        }
+
+        visualize();
+    }
+);
+
