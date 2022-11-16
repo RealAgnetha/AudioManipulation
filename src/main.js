1ddda128d146acfffbb6adfa674035ad7e663a78
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-11-16 19:18:20 +0100

    first steps

--- src/main.js
+++ src/main.js
@@ -5,25 +5,20 @@
 const lowerBandThreshold = 350;
 const higherBandThreshold = 8000;
 
-// instigate our audio context
 let audioCtx;
-
-
-// load some sound
 const audioElement = document.querySelector("audio");
 let track;
 
 const playButton = document.querySelector(".tape-controls-play");
 
-// play pause audio
+// play pause btn
 playButton.addEventListener(
     "click",
     () => {
         if (!audioCtx) {
             init();
         }
-
-        // check if context is in suspended state (autoplay policy)
+        // autoplay policy
         if (audioCtx.state === "suspended") {
             audioCtx.resume();
         }
@@ -35,15 +30,13 @@ playButton.addEventListener(
             audioElement.pause();
             playButton.dataset.playing = "false";
         }
-
-        // Toggle the state between play and not playing
         let state = playButton.getAttribute("aria-checked") === "true";
         playButton.setAttribute("aria-checked", state ? "false" : "true");
     },
     false
 );
 
-// If track ends
+//after track
 audioElement.addEventListener(
     "ended",
     () => {
@@ -53,15 +46,17 @@ audioElement.addEventListener(
     false
 );
 
+
 const diff = higherBandThreshold - lowerBandThreshold;
 const centerFreq = higherBandThreshold - (1 / 2) * diff;
 
 function init() {
     audioCtx = new AudioContext();
+
+    //le filters
     let lowFilter = new BiquadFilterNode(audioCtx, {type: 'lowshelf', frequency: lowerBandThreshold});
     let midFilter = new BiquadFilterNode(audioCtx, {type: 'bandpass', frequency: centerFreq, Q: 1});
     let highFilter = new BiquadFilterNode(audioCtx, {type: 'highshelf', frequency: higherBandThreshold})
-
 
     Lows.oninput = () => lowFilter.gain.value = Lows.value;
     Mids.oninput = () => midFilter.gain.value = Mids.value;
@@ -71,6 +66,36 @@ function init() {
     track = new MediaElementAudioSourceNode(audioCtx, {
         mediaElement: audioElement,
     });
+
+    //le analyser
+    const analyserContainer = document.getElementById("visualizer-container");
+    const canvas = document.getElementById("canvas1");
+    const canvasCtx = canvas.getContext('2d');
+
+    let analyser = track.createAnalyser();
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x = 0;
+    
+    function visualize() {
+        x=0;
+        canvasCtx.clearRect(0,0, canvas.width, canvas.height); //clear old frames
+        analyser.getByteFrequencyData(dataArray);
+        
+        for(let i=0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            canvasCtx.fillStyle = 'black';
+            canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth;
+        }
+        requestAnimationFrame(visualize);
+        
+    }
+visualize();
 
     // Create the node that controls the volume.
     const gainNode = new GainNode(audioCtx);
@@ -86,7 +111,8 @@ function init() {
 
     // connect graph
     gainNode.connect(audioCtx.destination);
-    track.connect(lowFilter);
+    track.connect(analyserNode);
+    analyser.connect(lowFilter);
     lowFilter.connect(midFilter);
     midFilter.connect(highFilter);
     highFilter.connect(gainNode);
