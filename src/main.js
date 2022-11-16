dc28ea709ee83208a3551dac61761cffb077879a
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-11-16 22:49:48 +0100

    fix visualisation

--- src/main.js
+++ src/main.js
@@ -10,21 +10,96 @@ let track;
 
 const audioElement = document.querySelector("audio");
 const playButton = document.getElementById("play-btn");
-const visualizeBtn = document.getElementById("viz-btn");
 
-//for bandpass 
+// for bandpass 
 const diff = higherBandThreshold - lowerBandThreshold;
 const centerFreq = higherBandThreshold - (1 / 2) * diff;
 
 
+
+// play pause btn 
+playButton.addEventListener(
+    'click',
+    () => {
+        if (!audioCtx) {
+            init();
+        }
+        // check if context is in suspended state (autoplay policy)
+        if (audioCtx.state === "suspended") {
+            audioCtx.resume();
+        }
+        if (playButton.dataset.playing === "false") {
+            audioElement.play();
+            playButton.dataset.playing = "true";
+            startVisualizing();
+            // if track is playing pause it
+        } else if (playButton.dataset.playing === "true") {
+            audioElement.pause();
+            playButton.dataset.playing = "false";
+        }
+
+        // Toggle the state between play and not playing
+        let state = playButton.getAttribute("aria-checked") === "true";
+        playButton.setAttribute("aria-checked", state ? "false" : "true");
+    },
+    false
+);
+
+// after track
+audioElement.addEventListener(
+    "ended",
+    () => {
+        playButton.dataset.playing = "false";
+        playButton.setAttribute("aria-checked", "false");
+    },
+    false
+);
+
+// for visualisation
+const canvas = document.getElementById("canvas1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const canvasCtx = canvas.getContext('2d');
+let analyser;
+
+//visualizeBtn.addEventListener("click", function () {
+function startVisualizing() {
+    analyser = audioCtx.createAnalyser();
+    //const analyser = new AnalyserNode(audioCtx);
+
+    track.connect(analyser);
+    analyser.connect(audioCtx.destination);
+
+    analyser.fftSize = 2048;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x = 0;
+
+    function visualize() {
+        x = 0;
+        canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            canvasCtx.fillStyle = 'black';
+            canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth;
+        }
+        requestAnimationFrame(visualize);
+    }
+    visualize();
+}
+
 function init() {
     audioCtx = new AudioContext();
-
     track = new MediaElementAudioSourceNode(audioCtx, {
         mediaElement: audioElement,
     });
 
-    //le filters
+    // le filters
     let lowFilter = new BiquadFilterNode(audioCtx, {type: 'lowshelf', frequency: lowerBandThreshold});
     let midFilter = new BiquadFilterNode(audioCtx, {type: 'bandpass', frequency: centerFreq, Q: 1});
     let highFilter = new BiquadFilterNode(audioCtx, {type: 'highshelf', frequency: higherBandThreshold})
@@ -51,85 +126,6 @@ function init() {
     lowFilter.connect(midFilter);
     midFilter.connect(highFilter);
     highFilter.connect(gainNode);
-
 }
 
-// play pause btn 
-playButton.addEventListener(
-    "click",
-    () => {
-        if (!audioCtx) {
-            init();
-        }
-
-        // check if context is in suspended state (autoplay policy)
-        if (audioCtx.state === "suspended") {
-            audioCtx.resume();
-        }
-        if (playButton.dataset.playing === "false") {
-            audioElement.play();
-            playButton.dataset.playing = "true";
-            // if track is playing pause it
-        } else if (playButton.dataset.playing === "true") {
-            audioElement.pause();
-            playButton.dataset.playing = "false";
-        }
-
-        // Toggle the state between play and not playing
-        let state = playButton.getAttribute("aria-checked") === "true";
-        playButton.setAttribute("aria-checked", state ? "false" : "true");
-    },
-    false
-);
-
-//after track
-audioElement.addEventListener(
-    "ended",
-    () => {
-        playButton.dataset.playing = "false";
-        playButton.setAttribute("aria-checked", "false");
-    },
-    false
-);
-
-//for visualisation
-const container = document.getElementById("visualisation");
-const canvas = document.getElementById("canvas1");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const canvasCtx = canvas.getContext('2d');
-let analyser;
-
-visualizeBtn.addEventListener("click", function () {
-
-        analyser = audioCtx.createAnalyser();
-
-        track.connect(analyser);
-        analyser.connect(audioCtx.destination);
-
-        analyser.fftSize = 2048;
-        const bufferLength = analyser.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-
-        const barWidth = canvas.width / bufferLength;
-        let barHeight;
-        let x = 0;
-
-        function visualize() {
-            x = 0;
-            canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-            analyser.getByteFrequencyData(dataArray);
-
-            for (let i = 0; i < bufferLength; i++) {
-                barHeight = dataArray[i];
-                canvasCtx.fillStyle = 'black';
-                canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-                x += barWidth;
-            }
-            requestAnimationFrame(visualize);
-        }
-
-        visualize();
-    }
-);
 
