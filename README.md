836f225a710fe58d81b0f0f320352efc80902fcd
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-11-16 17:53:14 +0100

    init and 1+2

---
+++ README.md
@@ -0,0 +1,22 @@
+# Programmieraufgabe 02: Audiomanipulation und -analyse
+
+## Einleitung
+
+Nutzen Sie die Vorlage und die in der Übung kennengelernten Technologien (insbesondere die Web-Audio-API) um ein einfaches Audio-Analyse-Tool zu programmieren.
+
+**2 Punkt** gibt es für allgemeine Code-Qualität.
+
+Bitte laden Sie Ihre Lösung als **ZIP**-Datei im dafür vorgesehenen Bereich im Moodle hoch.
+
+## Teilaufgabe 1 (6 Punkte)
+
+Entwerfen Sie einen einfachen Audio-Webplayer, welcher es erlaubt ein (statisches) Soundfile abzuspielen und grundlegende Steuermöglichkeiten erlaubt (mindestens Play, Pause, Volume).
+
+## Teilaufgabe 2 (6 Punkte)
+
+Erweitern Sie die Funktionalitäten ihres Prototyps um Filtermöglichkeiten, die einen einfachen Dreiband-Equalizer umsetzen. Dieser soll es erlauben die Tiefen, Mitten und Höhen zu verstärken bzw. abzuschwächen. Die Filter sollen über die UI steuerbar sein.
+Recherchieren / Experimentieren Sie eigenständig nach **sinnvollen Grenzwerten** für diese 3 Bereiche. Und nutzen Sie die in `main.js` vorbereiteten Variablen für diese.
+
+## Teilaufgabe 3 (6 Punkte)
+
+Erweitern Sie die grafische Oberfläche um eine Visualisierung, welche den aktuellen Peak der 3 Bänder des Equalizers aus Teilaufgabe 2 visualisiert. Die Visualisierung soll auch den _aktuellen Zustand der Filter_ mitberücksichtigen.
---
+++ src/main.js
@@ -0,0 +1,94 @@
+// Thresholds of the 3-band-equalizer
+//
+// Please use these variables for an 
+// easier review on my part
+const lowerBandThreshold = 350;
+const higherBandThreshold = 8000;
+
+
+// instigate our audio context
+let audioCtx;
+
+
+// load some sound
+const audioElement = document.querySelector("audio");
+let track;
+
+const playButton = document.querySelector(".tape-controls-play");
+
+// play pause audio
+playButton.addEventListener(
+    "click",
+    () => {
+        if (!audioCtx) {
+            init();
+        }
+
+        // check if context is in suspended state (autoplay policy)
+        if (audioCtx.state === "suspended") {
+            audioCtx.resume();
+        }
+        if (playButton.dataset.playing === "false") {
+            audioElement.play();
+            playButton.dataset.playing = "true";
+            // if track is playing pause it
+        } else if (playButton.dataset.playing === "true") {
+            audioElement.pause();
+            playButton.dataset.playing = "false";
+        }
+
+        // Toggle the state between play and not playing
+        let state = playButton.getAttribute("aria-checked") === "true";
+        playButton.setAttribute("aria-checked", state ? "false" : "true");
+    },
+    false
+);
+
+// If track ends
+audioElement.addEventListener(
+    "ended",
+    () => {
+        playButton.dataset.playing = "false";
+        playButton.setAttribute("aria-checked", "false");
+    },
+    false
+);
+
+const diff = higherBandThreshold-lowerBandThreshold;
+const centerFreq = higherBandThreshold-(1/2)*diff;
+function init() {
+    audioCtx = new AudioContext();
+    let lowFilter = new BiquadFilterNode(audioCtx, {type: 'lowshelf', frequency: lowerBandThreshold});
+    let midFilter = new BiquadFilterNode(audioCtx, {type: 'bandpass', frequency: centerFreq, Q: 1});
+    let highFilter = new BiquadFilterNode(audioCtx, {type: 'highshelf', frequency: higherBandThreshold})
+
+  
+    Lows.oninput = () => lowFilter.gain.value = Lows.value;
+    Mids.oninput = () => midFilter.gain.value = Mids.value;
+    Highs.oninput = () => highFilter.gain.value = Highs.value;
+
+
+    track = new MediaElementAudioSourceNode(audioCtx, {
+        mediaElement: audioElement,
+    });
+
+    // Create the node that controls the volume.
+    const gainNode = new GainNode(audioCtx);
+
+    const volumeControl = document.querySelector('[data-action="volume"]');
+    volumeControl.addEventListener(
+        "input",
+        () => {
+            gainNode.gain.value = volumeControl.value;
+        },
+        false
+    );
+
+    // connect graph
+    gainNode.connect(audioCtx.destination);
+    track.connect(lowFilter);
+    lowFilter.connect(midFilter);
+    midFilter.connect(highFilter);
+    highFilter.connect(gainNode);
+
+}
